5b66624722389e56fe5a7bee53733c5a80605b88
Jéssica <jessicaspf@gmail.com>
2024-03-13 22:01:10 -0300

    Criacao de estrutra de dados

--- backend/sistema_vendas.sql
+++ backend/sistema_vendas.sql
@@ -51,3 +51,75 @@ UsuarioAtualizacao INT,
 Ativo TINYINT(1) DEFAULT 1,
 INDEX idx_nome (Nome) 
 );
+
+-- Tabela Pedido
+CREATE TABLE IF NOT EXISTS Pedido (
+Id INT AUTO_INCREMENT PRIMARY KEY,
+ClienteID INT,
+DataPedido DATETIME,
+FormaPagamentoId INT,
+Status VARCHAR(50),
+DataCriacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+DataAtualizacao DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+UsuarioAtualizacao INT,
+FOREIGN KEY (ClienteID) REFERENCES Cliente(Id),
+FOREIGN KEY (FormaPagamentoId) REFERENCES FormaPagamento(Id)
+);
+
+-- Tabela ItemPedido
+CREATE TABLE IF NOT EXISTS ItemPedido (
+	Id INT AUTO_INCREMENT PRIMARY KEY,
+    PedidoId INT,
+    ProdutoId INT,
+    Quantidade INT,
+    DataCriacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+	DataAtualizacao DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	UsuarioAtualizacao INT,
+    FOREIGN KEY (PedidoId) REFERENCES Pedido(Id),
+    FOREIGN KEY (ProdutoId) REFERENCES Produto(Id)
+);
+
+-- Tabela GrupoUsuario
+CREATE TABLE IF NOT EXISTS GrupoUsuario (
+Id INT AUTO_INCREMENT PRIMARY KEY,
+Nome VARCHAR(100) NOT NULL,
+Descricao TEXT,
+DataCriacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+DataAtualizacao DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+UsuarioAtualizacao INT,
+Ativo TINYINT(1) DEFAULT 1
+);
+
+-- Tabela Permissao
+CREATE TABLE IF NOT EXISTS Permissao (
+Id INT AUTO_INCREMENT PRIMARY KEY,
+Nome VARCHAR(100) NOT NULL,
+Descricao TEXT,
+DataCriacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+DataAtualizacao DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+UsuarioAtualizacao INT,
+Ativo TINYINT(1) DEFAULT 1);
+
+-- Tabela PermissaoGrupo
+CREATE TABLE IF NOT EXISTS PermissaoGrupo (
+PermissaoID INT,
+GrupoUsuarioID INT,
+PRIMARY KEY (PermissaoID, GrupoUsuarioID),
+FOREIGN KEY (PermissaoID) REFERENCES Permissao(Id),
+FOREIGN KEY (GrupoUsuarioID) REFERENCES GrupoUsuario(Id));
+
+-- Tabela Usuario
+CREATE TABLE IF NOT EXISTS Usuario (
+Id INT AUTO_INCREMENT PRIMARY KEY,
+NomeUsuario VARCHAR(50) NOT NULL,
+Senha VARCHAR(100) NOT NULL,
+Email VARCHAR(100),
+GrupoUsuarioID INT,
+Ativo TINYINT(1) DEFAULT 1,
+DataCriacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+DataAtualizacao DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+UsuarioAtualizacao INT,    UNIQUE (NomeUsuario), 
+-- Restrição UNIQUE na coluna NomeUsuario
+UNIQUE (Email), 
+-- Restrição UNIQUE na coluna Email
+FOREIGN KEY (GrupoUsuarioID) REFERENCES GrupoUsuario(Id));
