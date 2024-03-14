6a2b3aa87f0a501c46327b4d59ac542a3a4c2bfd
Jéssica <jessicaspf@gmail.com>
2024-03-13 21:34:08 -0300

    Atualizando projeto de bd

---
+++ backend/sistema_vendas.sql
@@ -0,0 +1,53 @@
+-- Criação do banco de dados
+CREATE DATABASE IF NOT EXISTS sistema_vendas;
+USE sistema_vendas;
+
+-- Tabela Categoria
+CREATE TABLE IF NOT EXISTS Categoria (
+	Id INT  AUTO_INCREMENT PRIMARY KEY,
+    Nome VARCHAR(100) NOT NULL,
+    Descricao TEXT,
+    DataCriacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+    DataAtualizacao DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    UsuarioAtualizacao INT,
+    Ativo TINYINT(1) DEFAULT 1
+);
+
+-- Tabela FormaPagamento
+CREATE TABLE IF NOT EXISTS FormaPagamento (
+    Id INT  AUTO_INCREMENT PRIMARY KEY,
+    Nome VARCHAR(100) NOT NULL,
+    Descricao TEXT,
+    DataCriacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+    DataAtualizacao DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    UsuarioAtualizacao INT,
+    Ativo TINYINT(1) DEFAULT 1
+);
+
+-- Tabela Produto
+CREATE TABLE IF NOT EXISTS Produto (
+Id INT AUTO_INCREMENT PRIMARY KEY,
+Nome VARCHAR(100) NOT NULL,
+Descricao TEXT,
+Preco DECIMAL(10,2) NOT NULL,
+CategoriaID INT,
+DataCriacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+DataAtualizacao DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+UsuarioAtualizacao INT,
+Ativo TINYINT(1) DEFAULT 1,
+INDEX idx_nome (Nome), -- Adiciona índice nas colunas Nome
+CONSTRAINT fk_categoria_produto FOREIGN KEY (CategoriaID) REFERENCES Categoria(Id)
+);
+
+-- Tabela Cliente
+CREATE TABLE IF NOT EXISTS Cliente (
+Id INT AUTO_INCREMENT PRIMARY KEY,
+Nome VARCHAR(100) NOT NULL,
+Email VARCHAR(100),
+Telefone VARCHAR(20),
+DataCriacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+DataAtualizacao DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+UsuarioAtualizacao INT,
+Ativo TINYINT(1) DEFAULT 1,
+INDEX idx_nome (Nome) 
+);
